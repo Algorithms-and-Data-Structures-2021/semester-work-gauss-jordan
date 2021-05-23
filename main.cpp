bfbee24b95edd512270bb0046547cbc36f924d4c
Ibragimov Timur <i.timur@mail.ru>
2021-05-23 16:48:20 +0300

    Algorithm is finished

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <vector>
+#include "gauss_jordan_method.hpp"
 
-#include "data_structure.hpp"
+#define SIZE 10
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  for (int index = 0; index < argc; index++) {
-    cout << index << ": " << argv[index] << endl;
+  vector<float> result =  gauss();
+
+  for (int i = 0; i < result.size(); i++) {
+    cout << result.at(i) << " !!!" << endl;
   }
+
   return 0;
 }
